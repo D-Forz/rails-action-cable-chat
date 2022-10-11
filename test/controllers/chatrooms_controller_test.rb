f706f248195ea2a2f1b9e7bad7a6343e51d1fcfe
duvan <duvanfoz@hotmail.com>
2022-10-11 09:45:35 -0500

    test: chatroom controller

--- test/controllers/chatrooms_controller_test.rb
+++ test/controllers/chatrooms_controller_test.rb
@@ -4,4 +4,10 @@ class ChatroomsControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+  test "should get show if user is signed in" do
+    sign_in users(:jhon)
+    get chatroom_path(chatrooms(:general))
+    assert_response :success
+    assert_select "h1", "#general"
+  end
 end
